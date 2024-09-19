5f5964bd29c111f36a61b50eb223b137f1119dd1
Rivaric <helium@internet.ru>
2024-09-20 00:38:11 +0300

    ✨ Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.ORIGIN,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .addBearerAuth()
