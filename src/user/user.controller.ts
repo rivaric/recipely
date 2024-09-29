c221cde6d303bd4ae3876eda3899a54312328b4a
Rivaric <helium@internet.ru>
2024-09-29 20:31:07 +0300

    🔥 Remove refresh token whene get user me

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -31,7 +31,7 @@ export class UserController {
 
     const user = await this.userService.findOne(userId);
 
-    const { password, ...rest } = user;
+    const { password, refreshToken, ...rest } = user;
 
     return rest;
   }
